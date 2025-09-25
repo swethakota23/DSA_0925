3b978ad67022ec10f5e5d86162e0f0bf6fe3de79
Swetha Kota <163826796+swethakota23@users.noreply.github.com>
2025-09-25 13:26:28 -0700

    Time: 2 ms (80.84%) | Memory: 45.6 MB (29.40%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int ans = Integer.MAX_VALUE;
+        int[][] dp= new int[n][n];
+        for(int[] i:dp){
+            Arrays.fill(i,Integer.MAX_VALUE);
+        }
+        for (int j = 0; j < n; j++) {
+            ans = Math.min(ans, minFallRecur(matrix, n - 1, j,dp));
+        }
+        return ans;
+    }
+
+    private int minFallRecur(int[][] matrix, int i, int j,int[][] dp) {
+        // Out of bounds
+        if (j < 0 || j >= matrix.length) {
+            return Integer.MAX_VALUE;
+        }
+        if(dp[i][j]<Integer.MAX_VALUE) return dp[i][j];
+
+        // Base case
+        if (i == 0) {
+            return dp[i][j]=matrix[0][j];
+        }
+
+        // Use long to avoid overflow
+        long u  = (long) matrix[i][j] + minFallRecur(matrix, i - 1, j,dp);
+        long ld = (long) matrix[i][j] + minFallRecur(matrix, i - 1, j - 1,dp);
+        long rd = (long) matrix[i][j] + minFallRecur(matrix, i - 1, j + 1,dp);
+
+        return dp[i][j]=(int) Math.min(u, Math.min(ld, rd));
+    }
+}
