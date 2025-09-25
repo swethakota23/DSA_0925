2cf31e5d401a3056c65f60eed1fd669608c5276a
Swetha Kota <163826796+swethakota23@users.noreply.github.com>
2025-09-25 14:12:43 -0700

    Time: 8 ms (31.24%) | Memory: 45.2 MB (88.60%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -3,32 +3,53 @@ class Solution {
         int n = matrix.length;
         int ans = Integer.MAX_VALUE;
         int[][] dp= new int[n][n];
-        for(int[] i:dp){
-            Arrays.fill(i,Integer.MAX_VALUE);
+        // for(int[] i:dp){
+        //     Arrays.fill(i,Integer.MAX_VALUE);
+        // }
+        // for (int j = 0; j < n; j++) {
+        //     ans = Math.min(ans, minFall(matrix, n - 1, j,dp));
+        // }
+        for(int j=0;j<n;j++){
+            dp[0][j]=matrix[0][j];
         }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                long u  = matrix[i][j] + dp[i-1][j];
+                long ld = (j > 0) ? matrix[i][j] + dp[i-1][j-1] : Integer.MAX_VALUE;
+                long rd = (j<n-1) ? matrix[i][j] + dp[i-1][j+1] : Integer.MAX_VALUE;
+               
+                dp[i][j]=(int)Math.min(u, Math.min(ld,rd));
+            }
+        }
+        int mini = Integer.MAX_VALUE;
         for (int j = 0; j < n; j++) {
-            ans = Math.min(ans, minFall(matrix, n - 1, j,dp));
+            mini = Math.min(mini, dp[n - 1][j]);
         }
-        return ans;
+
+        return mini;
     }
 
-    private int minFall(int[][] matrix, int i, int j,int[][] dp) {
-        // Out of bounds
-        if (j < 0 || j >= matrix.length) {
-            return Integer.MAX_VALUE;
-        }
-        if(dp[i][j]<Integer.MAX_VALUE) return dp[i][j];
+    // private int minFall(int[][] matrix, int i, int j,int[][] dp) {
+    //     // Out of bounds
+    //     if (j < 0 || j >= matrix.length) {
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     if(dp[i][j]<Integer.MAX_VALUE) return dp[i][j];
 
-        // Base case
-        if (i == 0) {
-            return dp[i][j]=matrix[0][j];
-        }
+    //     // Base case
+    //     if (i == 0) {
+    //         return dp[i][j]=matrix[0][j];
+    //     }
 
-        // Use long to avoid overflow
-        long u  = (long) matrix[i][j] + minFall(matrix, i - 1, j,dp);
-        long ld = (long) matrix[i][j] + minFall(matrix, i - 1, j - 1,dp);
-        long rd = (long) matrix[i][j] + minFall(matrix, i - 1, j + 1,dp);
+    //     // Use long to avoid overflow
+    //     long u  = (long) matrix[i][j] + minFall(matrix, i - 1, j,dp);
+    //     long ld = (long) matrix[i][j] + minFall(matrix, i - 1, j - 1,dp);
+    //     long rd = (long) matrix[i][j] + minFall(matrix, i - 1, j + 1,dp);
 
-        return dp[i][j]=(int) Math.min(u, Math.min(ld, rd));
-    }
+    //     return dp[i][j]=(int) Math.min(u, Math.min(ld, rd));
+    // }
+
+    
+    
+
 }
